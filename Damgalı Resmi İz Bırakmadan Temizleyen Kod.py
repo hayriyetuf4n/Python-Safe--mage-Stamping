98fa7a90e4d2488b3913c154d7140fc7648a68e0
Hayriye Tufan <hayriye.t314@gmail.com>
2026-03-25 20:55:15 +0300

    Update Damgalı Resmi İz Bırakmadan Temizleyen Kod.py

--- Damgalı Resmi İz Bırakmadan Temizleyen Kod.py
+++ Damgalı Resmi İz Bırakmadan Temizleyen Kod.py
@@ -1,34 +1,47 @@
 import os
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
-#Temiz olan resmin yolu:
-orijinal_temiz_resim = r"C:\Users\hayri\OneDrive\Masaüstü\nergis.jpg.jpg"
+#
+orijinal_yol = r"C:\Users\siz\OneDrive\Masaüstü\nergis.jpg.jpg"
+cikti_yol = r"C:\Users\siz\OneDrive\Masaüstü\ucan_ev_tam_orta.jpg"
 
-# Üzerine yazı eklenen(bozulmuş) resmin yolu:
-yazili_bozuk_resim =r"C:\Users\siz\OneDrive\Masaüstü\ucan_ev_tam_orta.jpg"
+def yazi_ekle_tam_orta():
+    if not os.path.exists(orijinal_yol): #os.path.exists=yazıyı yazmadan önce dosya gerçekten o konumda mı diye bakıyor.
+        print("HATA: Orijinal resim bulunamadı!")  #değilse uyarı veriyor.
+        return
 
-def yaziyi_sil_ve_orijinale_don():
-    print("--- İşlem Başlatıldı: Yazı Kaldırılıyor ---")
+    try:
+        img = Image.open(orijinal_yol).convert("RGB")#Resmi açıp renk modunu RGB ye sabitliyor,renk kanallarını eşitler.
+        draw = ImageDraw.Draw(img)  #resmin üstüne sanal bir katman koyup oraya yazı yazar
+        W, H = img.size
 
-    if os.path.exists(yazili_bozuk_resim): #Yazılı olan dosya gerçekten var mı diye bakar
+       
         try:
-            os.remove(yazili_bozuk_resim) #Yazılı olan dosyayı bilgisayardan tamamen siler
-            print(f"BAŞARILI: Yazılı dosya ({yazili_bozuk_resim}) sistemden temizlendi.")
-        except Exception as e:  #Hata olursa bunun sayesinde yazdırır.
-            print(f"HATA: Dosya silinirken bir sorun oluştu: {e}")
-            return                                       # "e" hatayı isimlendirir.
+            font = ImageFont.truetype("arial.ttf", 80)  #yazı tipi yüklüyor ve boyutunu 80 piksel ayarlıyor
+        except:
+            # Arial bulunamazsa standart fontu kullanıyor
+            font = ImageFont.load_default()
 
-        #Orijinal temiz dosyayı tekrar kullanıcıya gösterme
-        if os.path.exists(orijinal_temiz_resim): #dosya var mı diye bakıyor
-            print("BİLGİ: Orijinal resminiz zaten güvendeydi,resim açılıyor")
-            temiz_img = Image.open(orijinal_temiz_resim)
-            temiz_img.show() #Temiz, yazısız hali ekranda açılır
-            print(" Resim eski haline getirildi")
-        else:
-            print("UYARI: Yazılı dosyayı silindi ama orijinal 'nergis.jpg' dosyasını bulunamadı.")
-    
-    else:
-        print("BİLGİ:Yazılı bir dosya yok, resim zaten temiz.")
+        mesaj = "UCAN EV VE BALONLAR"
 
-# --- ÇALIŞTIRMA ---
-yaziyi_sil_ve_orijinale_don()
+        left, top, right, bottom = draw.textbbox((0, 0), mesaj, font=font)#tam ortayı hesaplayıp yazıyı yazdıracak
+        w = right - left
+        h = bottom - top
+        
+        x = (W - w) / 2    #Tam orta koordinatlarını hesaplıyor
+        y = (H - h) / 2
+
+        # Yazıyı ekle (Önce gölge, sonra beyaz yazı)
+        draw.text((x+4, y+4), mesaj, font=font, fill=(0, 0, 0)) # Gölge şeklinde yazı,hesaplanan konummun 4 piksel sağına ve soluna
+        draw.text((x, y), mesaj, font=font, fill=(255, 255, 255)) # Ana yazı,beyaz olacak şekilde
+                            #yazı tipi-boyutu  #beyaz renk kodu
+        # Kaydetme ve Gösterme
+        img.save(cikti_yol)
+        print(f"BAŞARILI: Yazı eklendi. Dosya: {cikti_yol}")
+        img.show()
+
+    except Exception as e:
+        print(f"Bir hata oluştu: {e}")
+
+# Fonksiyonu çalıştır
+yazi_ekle_tam_orta()
